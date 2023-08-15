401d7d962fea39b0667f427fd144907f7cb58a95
sergio-ac <japtrol@gmail.com>
2023-08-15 17:43:04 -0600

    estructura principal sin contenido

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.getElementById("menu-toggle");
+    const mobileMenu = document.getElementById("mobile-menu");
+
+    menuToggle.addEventListener("click", function() {
+        mobileMenu.classList.toggle("active");
+    });
+});
